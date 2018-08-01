6074025c9ec93596d5826defc8777e118782cbf3
sl4gr00m <sl4gr00m@gmail.com>
2018-08-01 14:21:43 -0300

    Create (failing) tests for Maybe<T>

---
+++ wimm.Secundatives/Maybe.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace wimm.Secundatives
+{
+    /// <summary>
+    /// Minimum implementation for tests to compile.
+    /// </summary>
+    public struct Maybe<T> : IEquatable<Maybe<T>>
+    {
+        public readonly static Maybe<T> None;
+
+        public T Value { get; }
+
+        public Maybe(T value) => throw new NotImplementedException();
+
+        public static bool operator ==(Maybe<T> left, Maybe<T> right) => throw new NotImplementedException();
+
+        public static bool operator !=(Maybe<T> left, Maybe<T> right) => throw new NotImplementedException();
+
+        public override bool Equals(object obj) => throw new NotImplementedException();
+
+        public bool Equals(Maybe<T> other) => throw new NotImplementedException();
+
+        public override int GetHashCode() => throw new NotImplementedException();
+    }
+}
