ad40e3dd1638ea06998914d58553041b977e058c
Chris Nantau <Chris.Nantau@gmail.com>
2018-10-03 23:09:01 -0300

    Implementation of ExpectException, Expect and UnwrapOr +semver:feature

---
+++ wimm.Secundatives/Exceptions/ExpectException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wimm.Secundatives.Exceptions
+{
+    public class ExpectException : Exception
+    {
+
+        public ExpectException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ wimm.Secundatives/Extensions/MaybeExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wimm.Secundatives.Exceptions;
+
+namespace wimm.Secundatives.Extensions
+{
+    public static class MaybeExtensions
+    {
+        public static T Expect<T>(this Maybe<T> maybe)
+        {
+            return maybe.Expect($"Expected value in {nameof(Maybe<T>)}");
+        }
+
+        public static T Expect<T>(this Maybe<T> maybe, string message)
+        {
+            if (message == null)
+                throw new ArgumentNullException(nameof(message));
+            if (string.IsNullOrWhiteSpace(message))
+                throw new ArgumentException("Parameter must not be whitespace or empty", nameof(message));
+
+            return maybe.Exists ? maybe.Value : throw new ExpectException(message);
+        }
+
+        public static T UnwrapOr<T>(this Maybe<T> maybe, T defaultValue)
+        {
+            return maybe.Exists ? maybe.Value : defaultValue;
+        }
+
+        public static T UnwrapOr<T>(this Maybe<T> maybe, Func<T> func)
+        {
+            return maybe.Exists ? maybe.Value : func();
+        }
+    }
+}
