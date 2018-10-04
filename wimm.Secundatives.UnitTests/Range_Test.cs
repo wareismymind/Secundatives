66978dbd3af7d1cf4f59b70befb37f57e2e78dbe
slagroom <sl4gr00m@gmail.com>
2018-10-03 21:24:53 -0300

    Create Range<T> type with bool Includes(T)

---
+++ wimm.Secundatives.UnitTests/Range_Test.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using Xunit;
+
+namespace wimm.Secundatives.UnitTests
+{
+    public class Range_Test
+    {
+        private const string _lessThanMin = "a";
+        private const string _min = "b";
+        private const string _betweenMinAndMax = "c";
+        private const string _max = "d";
+        private const string _greaterThanMax = "e";
+
+        [Fact]
+        public void ConstructMinMax_NullMin_Throws()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() =>
+            {
+                var _ = new Range<string>(null, _max);
+            });
+
+            Assert.Equal("min", ex.ParamName);
+        }
+
+        [Fact]
+        public void ConstructMinMax_NullMax_Throws()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() =>
+            {
+                var _ = new Range<string>(_min, null);
+            });
+
+            Assert.Equal("max", ex.ParamName);
+        }
+
+        [Fact]
+        public void ConstructMinMax_MaxIsLessThanMin_Throws()
+        {
+            var ex = Assert.Throws<ArgumentException>(() =>
+            {
+                var _ = new Range<string>(_min, _lessThanMin);
+            });
+        }
+
+        [Fact]
+        public void ConstructMinMax_MaxIsMin_Constructs()
+        {
+            var _ = new Range<string>(_min, _min);
+        }
+
+        [Fact]
+        public void ConstructMinMax_MaxIsGreaterThanMin_Constructs()
+        {
+            var _ = new Range<string>(_min, _max);
+        }
+
+        [Theory]
+        [InlineData(false)]
+        [InlineData(true)]
+        public void ConstructMinMaxExclusive_NullMin_Throws(bool exclusive)
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() =>
+            {
+                var _ = new Range<string>(null, _max, exclusive);
+            });
+
+            Assert.Equal("min", ex.ParamName);
+        }
+
+        [Theory]
+        [InlineData(false)]
+        [InlineData(true)]
+        public void ConstructMinMaxExclusive_NullMax_Throws(bool exclusive)
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() =>
+            {
+                var _ = new Range<string>(_min, null, exclusive);
+            });
+
+            Assert.Equal("max", ex.ParamName);
+        }
+
+        [Theory]
+        [InlineData(false)]
+        [InlineData(true)]
+        public void ConstructMinMaxExclusive_MaxIsLessThanMin_Throws(bool exclusive)
+        {
+            var ex = Assert.Throws<ArgumentException>(() =>
+            {
+                var _ = new Range<string>(_min, _lessThanMin, exclusive);
+            });
+        }
+
+        [Fact]
+        public void ConstructMinMaxExclusive_NonExclusiveMaxIsMin_Constructs()
+        {
+            var _ = new Range<string>(_min, _max, false);
+        }
+
+        [Fact]
+        public void ConstructMinMaxExclusive_ExclusiveMaxIsMin_Throws()
+        {
+            var ex = Assert.Throws<ArgumentException>(() =>
+            {
+                var _ = new Range<string>(_min, _min, true);
+            });
+        }
+
+        [Theory]
+        [InlineData(false)]
+        [InlineData(true)]
+        public void ConstructMinMaxExclusive_MaxGreaterThanMin_Constructs(bool exclusive)
+        {
+            var _ = new Range<string>(_min, _max, exclusive);
+        }
+
+        [Fact]
+        public void Min_ConstructMinMax_IsMin()
+        {
+            var underTest = new Range<string>(_min, _max);
+            Assert.Equal(_min, underTest.Min);
+        }
+
+        [Theory]
+        [InlineData(false)]
+        [InlineData(true)]
+        public void Min_ConstructMinMaxExclusive_IsMin(bool exclusive)
+        {
+            var underTest = new Range<string>(_min, _max, exclusive);
+            Assert.Equal(_min, underTest.Min);
+        }
+
+        [Fact]
+        public void Max_ConstructMinMax_IsMax()
+        {
+            var underTest = new Range<string>(_min, _max);
+            Assert.Equal(_max, underTest.Max);
+        }
+
+        [Theory]
+        [InlineData(false)]
+        [InlineData(true)]
+        public void Max_ConstructMinMaxExclusive_IsMax(bool exclusive)
+        {
+            var underTest = new Range<string>(_min, _max, exclusive);
+            Assert.Equal(_max, underTest.Max);
+        }
+
+        [Fact]
+        public void Exclusive_ConstructMinMax_IsFalse()
+        {
+            var underTest = new Range<string>(_min, _max);
+            Assert.False(underTest.Exclusive);
+        }
+
+        [Theory]
+        [InlineData(false)]
+        [InlineData(true)]
+        public void Exclusive_ConstructMinMaxExclusive_IsExclusive(bool exclusive)
+        {
+            var underTest = new Range<string>(_min, _max, exclusive);
+            Assert.Equal(exclusive, underTest.Exclusive);
+        }
+
+        [Fact]
+        public void Includes_NullValue_Throws()
+        {
+            var underTest = new Range<string>(_min, _max);
+
+            var ex = Assert.Throws<ArgumentNullException>(() =>
+            {
+                var _ = underTest.Includes(null);
+            });
+
+            Assert.Equal("value", ex.ParamName);
+        }
+
+        [Fact]
+        public void Includes_NonExclusiveValueIsLessThanMin_False()
+        {
+            var underTest = new Range<string>(_min, _max, false);
+            Assert.False(underTest.Includes(_lessThanMin));
+        }
+
+        [Fact]
+        public void Includes_NonExclusiveValueIsMin_True()
+        {
+            var underTest = new Range<string>(_min, _max, false);
+            Assert.True(underTest.Includes(_min));
+        }
+
+        [Fact]
+        public void Includes_NonExclusiveValueIsBetweenMinAndMax_True()
+        {
+            var underTest = new Range<string>(_min, _max, false);
+            Assert.True(underTest.Includes(_betweenMinAndMax));
+        }
+
+        [Fact]
+        public void Includes_NonExclusiveValueIsMax_True()
+        {
+            var underTest = new Range<string>(_min, _max, false);
+            Assert.True(underTest.Includes(_max));
+        }
+
+        [Fact]
+        public void Includes_NonExclusiveValueIsGreaterThanMax_True()
+        {
+            var underTest = new Range<string>(_min, _max, false);
+            Assert.False(underTest.Includes(_greaterThanMax));
+        }
+
+        [Fact]
+        public void Includes_ExclusiveValueIsLessThanMin_False()
+        {
+            var underTest = new Range<string>(_min, _max, true);
+            Assert.False(underTest.Includes(_lessThanMin));
+        }
+
+        [Fact]
+        public void Includes_ExclusiveValueIsMin_False()
+        {
+            var underTest = new Range<string>(_min, _max, true);
+            Assert.False(underTest.Includes(_min));
+        }
+
+        [Fact]
+        public void Includes_ExclusiveValueIsBetweenMinAndMax_True()
+        {
+            var underTest = new Range<string>(_min, _max, true);
+            Assert.True(underTest.Includes(_betweenMinAndMax));
+        }
+
+        [Fact]
+        public void Includes_ExclusiveValueIsMax_False()
+        {
+            var underTest = new Range<string>(_min, _max, true);
+            Assert.False(underTest.Includes(_max));
+        }
+
+        [Fact]
+        public void Includes_ExclusiveValueIsGreaterThanMax_True()
+        {
+            var underTest = new Range<string>(_min, _max, true);
+            Assert.False(underTest.Includes(_greaterThanMax));
+        }
+    }
+}
---
+++ wimm.Secundatives/Range.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace wimm.Secundatives
+{
+    /// <summary>
+    /// A range for an <see cref="IComparable{T}"/> value.
+    /// </summary>
+    public class Range<T> where T : IComparable<T>
+    {
+        /// <summary>
+        /// The minimum value.
+        /// </summary>
+        public T Min { get; }
+
+        /// <summary>
+        /// The maximum value.
+        /// </summary>
+        public T Max { get; }
+
+        /// <summary>
+        /// Indicates whether <see cref="Min"/> and <see cref="Max"/> are excluded from the range.
+        /// When <c>false</c>, values be greater than or equal to <see cref="Min"/> and less than
+        /// or equal to <see cref="Max"/> are considered in range. When <c>true</c> values must be
+        /// strictly greater than <see cref="Min"/> and less than <see cref="Max"/>.
+        /// </summary>
+        public bool Exclusive { get; }
+
+        /// <summary>
+        /// Initializes a new, non-exclusive <see cref="Range{T}"/>.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="min"/> or <paramref name="max"/> is <c>null</c>.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// <paramref name="max"/> is less than <paramref name="min"/>.
+        /// </exception>
+        public Range(T min, T max) : this(min, max, false) { }
+
+        /// <summary>
+        /// Initializes a new, non-exclusive <see cref="Range{T}"/>.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="min"/> or <paramref name="max"/> is <c>null</c>.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// <paramref name="max"/> is less than <paramref name="min"/>, or
+        /// <paramref name="exclusive"/> is <c>true</c> and <paramref name="max"/> is equal to
+        /// <paramref name="min"/>.
+        /// </exception>
+        public Range(T min, T max, bool exclusive)
+        {
+            Min = Require(min, nameof(min));
+            Max = Require(max, nameof(max));
+            Exclusive = exclusive;
+
+            if (max.CompareTo(min) < 0)
+                throw new ArgumentException(
+                    $"The value of {nameof(max)} must not be less than the value of {nameof(min)}.");
+
+            if (exclusive && max.CompareTo(min) == 0)
+                throw new ArgumentException(
+                    $"The value of {nameof(max)} must be greater than the value of {nameof(min)} for exclusive ranges.");
+        }
+
+        /// <summary>
+        /// Indicates whether a value is included in the range.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="value"/> is <c>null</c>.
+        /// </exception>
+        public bool Includes(T value)
+        {
+            Require(value, nameof(value));
+
+            if (Min.CompareTo(value) > 0 || Max.CompareTo(value) < 0)
+                return false;
+
+            if (Exclusive && (Min.CompareTo(value) == 0 || Max.CompareTo(value) == 0))
+                return false;
+
+            return true;
+        }
+
+        private T Require(T value, string name)
+        {
+            if (value == null) throw new ArgumentNullException(name);
+            return value;
+        }
+    }
+}
