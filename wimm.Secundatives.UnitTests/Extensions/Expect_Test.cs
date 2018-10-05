4ee1a60a539a3091417b2ab4cca8a42ac3a6d901
Insomniak47 <Chris.Nantau@gmail.com>
2018-10-05 16:08:08 -0300

    Feature/maybe extensions (#10)
    * Tests for UnwrapOr and Expect
    
    * Implementation of ExpectException, Expect and UnwrapOr +semver:feature
    
    * Added documentation to UnwrapOr and Expect
    
    * Changed Expect and UnwrapOr one-liners to expression body
    
    * Fix for bug in test that tested Expect(string) instead of Expect()
    
    * Added explicit private qualifier for test message param

---
+++ wimm.Secundatives.UnitTests/Extensions/Expect_Test.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wimm.Secundatives.Exceptions;
+using wimm.Secundatives.Extensions;
+using Xunit;
+
+namespace wimm.Secundatives.UnitTests.Extensions
+{
+    public class Expect_Test
+    {
+        private const string _valid = "doot";
+        private const string _message = "this is a message";
+
+        [Fact]
+        public void ExpectMessage_NullMessage_Throws()
+        {
+            var underTest = new Maybe<string>(_valid);
+            Assert.Throws<ArgumentNullException>("message",() => underTest.Expect(null));
+        }
+
+        [Fact]
+        public void ExpectMessage_WhiteSpaceMessage_Throws()
+        {
+            var underTest = new Maybe<string>(_valid);
+            Assert.Throws<ArgumentException>("message",() => underTest.Expect(" \t\r\n"));
+        }
+
+
+        [Fact]
+        public void ExpectMessage_DoesNotExist_ThrowsWithMessage()
+        {
+            var underTest = new Maybe<string>();
+            var ex = Assert.Throws<ExpectException>(() => underTest.Expect(_message));
+
+            Assert.Equal(_message, ex.Message);
+        }
+
+        [Fact]
+        public void ExpectNoMessage_DoesNotExist_Throws()
+        {
+            var underTest = new Maybe<string>();
+            var ex = Assert.Throws<ExpectException>(() => underTest.Expect());
+        }
+
+        [Fact]
+        public void ExpectMessage_Exists_ReturnsValue()
+        {
+            var underTest = new Maybe<string>(_valid);
+            Assert.Equal(_valid, underTest.Expect(_message));
+        }
+
+        [Fact]
+        public void ExpectNoMessage_Exists_ReturnsValue()
+        {
+            var underTest = new Maybe<string>(_valid);
+            Assert.Equal(_valid, underTest.Expect());
+        }
+
+
+    }
+}
---
+++ wimm.Secundatives.UnitTests/Extensions/UnwrapOr_Test.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wimm.Secundatives.Extensions;
+using Xunit;
+
+namespace wimm.Secundatives.UnitTests.Extensions
+{
+    public class UnwrapOr_Test
+    {
+        private const string _valid = "doot";
+        private const string _default = "tood";
+        private Func<string> _defaultFunc = () => _default;
+
+        [Fact]
+        public void UnwrapOr_Value_ReturnsValue()
+        {
+            var underTest = new Maybe<string>(_valid);
+
+            Assert.Equal(_valid, underTest.UnwrapOr(_default));
+        }
+
+        [Fact]
+        public void UnwrapOrFunc_Value_ReturnsValue()
+        {
+            var underTest = new Maybe<string>(_valid);
+            Assert.Equal(_valid, underTest.UnwrapOr(_defaultFunc));
+        }
+
+        [Fact]
+        public void UnwrawpOrFunc_NoValue_ExecutesFunction()
+        {
+            var underTest = new Maybe<string>();
+            Assert.Equal(_default, underTest.UnwrapOr(_defaultFunc));
+        }
+
+
+        [Fact]
+        public void UnwrapOr_NoValue_ReturnsDefaultParam()
+        {
+            var underTest = new Maybe<string>();
+            Assert.Equal(_default, underTest.UnwrapOr(_default));
+        }
+    }
+}
