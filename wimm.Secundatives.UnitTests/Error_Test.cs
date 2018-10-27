9881a1c7fa10277aac2a192e9b58c6e50ef6abfd
Chris Nantau <Chris.Nantau@gmail.com>
2018-10-27 12:17:27 -0300

    Implementation of base Error type to be used in result

---
+++ wimm.Secundatives.UnitTests/Error_Test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace wimm.Secundatives.UnitTests
+{
+    
+    public class Error_Test
+    {
+        [Fact]
+        public void Construct_MessageNull_Throws()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Error(null));
+        }
+
+        [Fact]
+        public void Construct_MessageWhitespace_Throws()
+        {
+            TestHelpers.AssertThrowsIfWhitespace(x => new Error(x));
+        }
+
+        [Fact]
+        public void Construct_MessageValid_Constructs()
+        {
+            var err = new Error("I am a detailed error message: doot failed to construct");
+            Assert.NotNull(err.Message);
+        }
+    }
+}
---
+++ wimm.Secundatives/Error.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace wimm.Secundatives
+{
+    /// <summary>
+    /// Base class for Error types to be returned from results. 
+    /// </summary>
+    public class Error
+    {
+        /// <summary>
+        /// Constructs a new instance of <see cref="Error"/> with the specified message
+        /// </summary>
+        /// <param name="message"></param>
+        public Error(string message)
+        {
+            if (message == null)
+                throw new ArgumentNullException(nameof(message));
+            if (string.IsNullOrWhiteSpace(message))
+                throw new ArgumentException(
+                    "Error message must contain one or more non-whitespace characters", nameof(message));
+
+            Message = message;
+        }
+
+        /// <summary>
+        /// A detailed, non-null message describing the error. 
+        /// </summary>
+        public string Message { get; }
+    }
+}
