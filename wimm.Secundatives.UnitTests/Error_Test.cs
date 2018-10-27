d7731aff9907705da18999388ab1c68252b3d5f4
Insomniak47 <Chris.Nantau@gmail.com>
2018-10-27 12:59:19 -0300

    Feature/errors (#13)
    * Implementation of base Error type to be used in result
    
    * Finished up documentation on error class
    
    * Created and sealed ToString and added a debugging attribute. Also cleaned up usings

---
+++ wimm.Secundatives.UnitTests/Error_Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xunit;
+
+namespace wimm.Secundatives.UnitTests
+{
+
+    public class Error_Test
+    {
+        [Fact]
+        public void Construct_MessageNull_Throws()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Error(null));
+        }
+
+        [Fact]
+        public void Construct_MessageWhitespace_Throws()
+        {
+            TestHelpers.AssertThrowsIfWhitespace(x => new Error(x));
+        }
+
+        [Fact]
+        public void Construct_MessageValid_Constructs()
+        {
+            var err = new Error("I am a detailed error message: doot failed to construct");
+            Assert.NotNull(err.Message);
+        }
+    }
+}
---
+++ wimm.Secundatives/Error.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace wimm.Secundatives
+{
+    /// <summary>
+    /// Base class for Error types to be returned from results. 
+    /// </summary>
+    [DebuggerDisplay("{Message}")]
+    public class Error
+    {
+        /// <summary>
+        /// Constructs a new instance of <see cref="Error"/> with the specified message
+        /// </summary>
+        /// <param name="message"> The message to be included with the error </param>
+        /// <exception cref="ArgumentNullException"> <paramref name="message"/> is null </exception>
+        /// <exception cref="ArgumentException"> <paramref name="message"/> is whitespace </exception>
+        public Error(string message)
+        {
+            if (message == null)
+                throw new ArgumentNullException(nameof(message));
+            if (string.IsNullOrWhiteSpace(message))
+                throw new ArgumentException(
+                    "Error message must contain one or more non-whitespace characters", nameof(message));
+
+            Message = message;
+        }
+
+        /// <summary>
+        /// A detailed, non-null message describing the error. 
+        /// </summary>
+        public string Message { get; }
+
+        /// <summary>
+        /// Converts the <see cref="Error"/> into a <see cref="string"/>. 
+        /// </summary>
+        /// <returns> The formatted error message </returns>
+        public override sealed string ToString()
+        {
+            return Message;
+        }
+    }
+}
