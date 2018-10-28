5fd4515cb75cecec5816008f0adfebec43a40b71
Insomniak47 <Chris.Nantau@gmail.com>
2018-10-28 18:37:07 -0300

    Feature/variant ergo (#15)
    * Created tests and impl for support variant type to underpin result
    
    * Documentation typo fix
    
    * Finished implementing a missed test and returned Variant to class since there isn't a great default state
    
    * Adding missing readonly to UnwrapOr_Test
    
    * Using cleanup
    
    * Changed Is<W> to throw if W not in {U, T}
    
    * Removed heirarchy check from Is<W>
    
    * Implementation and tests of MapValue and MapAction
    
    +semver:feature

---
+++ wimm.Secundatives.UnitTests/Extensions/VariantMap_Test.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using wimm.Secundatives.Extensions;
+
+namespace wimm.Secundatives.UnitTests.Extensions
+{
+    public class VariantMap_Test
+    {
+        [Fact]
+        public void MapValue_ValueInt_ReturnsIntFuncResult()
+        {
+            var underTest = ConstructInt();
+            Assert.Equal(43, underTest.MapValue((x) => x + 1, x => x.Length));
+        }
+
+        [Fact]
+        public void MapValue_ValueString_ReturnsStringFuncResult()
+        {
+            var underTest = ConstructString();
+            Assert.Equal(4, underTest.MapValue((x) => x + 1, x => x.Length));
+        }
+
+        [Fact]
+        public void MapAction_ValueInt_ExecutesIntAction()
+        {
+            var underTest = ConstructInt();
+            int i = 0;
+            underTest.MapAction(x => i++, y => i--);
+            Assert.Equal(1, i);
+        }
+
+        [Fact]
+        public void MapAction_ValueString_ExecutesStringAction()
+        {
+            var underTest = ConstructString();
+            int i = 0;
+            underTest.MapAction(x => i++, y => i--);
+            Assert.Equal(-1, i);
+        }
+
+        public Variant<int,string> ConstructInt()
+        {
+            return new Variant<int, string>(42);
+        }
+
+        public Variant<int,string> ConstructString()
+        {
+            return new Variant<int, string>("doot");
+        }
+    }
+}
