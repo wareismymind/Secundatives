d01b364a7d5ffbf4df91a6fb8b2535331621de63
Unknown <Chris.Nantau@gmail.com>
2019-05-30 22:10:44 -0300

    Implementation and tests for an AsMaybe conversion
    +semver:feature

---
+++ wimm.Secundatives.UnitTests/Extensions/AsMaybe_Test.cs
@@ -0,0 +1,49 @@
+﻿using wimm.Secundatives.Extensions;
+using Xunit;
+
+namespace wimm.Secundatives.UnitTests.Extensions
+{
+    public class AsMaybe_Test
+    {
+        [Fact]
+        public void AsMaybeClass_NullValue_ReturnsNone()
+        {
+            string value = null;
+
+            var res = value.AsMaybe();
+
+            Assert.Equal(Maybe<string>.None, res);
+        }
+
+        [Fact]
+        public void AsMaybeClass_Value_ReturnsSome()
+        {
+            var value = "doot";
+            var res = value.AsMaybe();
+
+            Assert.True(res.Exists);
+        }
+
+        [Fact]
+        public void AsMaybeNullable_NullValue_ReturnsNone()
+        {
+            int? value = null;
+
+            var res = value.AsMaybe();
+
+            Assert.Equal(Maybe<int>.None, res);
+        }
+
+        [Fact]
+        public void AsMaybeNullable_Value_ReturnsSome()
+        {
+            int? value = 10;
+
+            var res = value.AsMaybe();
+
+            Assert.True(res.Exists);
+        }
+
+
+    }
+}
