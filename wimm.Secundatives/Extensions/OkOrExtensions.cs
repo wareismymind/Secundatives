cf0f542f5ac265059b3df0638db271cb8bd928fd
Chris Nantau <Chris.Nantau@gmail.com>
2019-10-22 17:50:53 -0300

    intitial tests and impl of okor

---
+++ wimm.Secundatives/Extensions/OkOrExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wimm.Secundatives
+{
+    public static class OkOrExtensions
+    {
+        public static Result<T, TError> OkOr<T, TError>(this Maybe<T> value, Func<TError> func)
+        {
+            if (value.Exists)
+                return value.Value;
+
+            return func();
+        }
+
+        public static Result<T, TError> OkOr<T, TError>(this Maybe<T> value, TError error)
+        {
+            if (value.Exists)
+                return value.Value;
+
+            return error;
+        }
+
+        public static async Task<Result<T, TError>> OkOr<T, TError>(this Task<Maybe<T>> value, TError err)
+        {
+            var v = await value;
+            return v.OkOr(err);
+        }
+
+        public static async Task<Result<T, TError>> OkOr<T, TError>(this Task<Maybe<T>> value, Func<TError> func)
+        {
+            var v = await value;
+            return v.OkOr(func);
+
+        }
+
+    }
+}
