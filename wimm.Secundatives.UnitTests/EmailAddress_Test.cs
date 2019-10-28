12c0e42211cc2aa441204e7cc8e631d5b80d1e03
unknown <sl4gr00m@gmail.com>
2019-10-27 22:33:34 -0300

    wip

---
+++ wimm.Secundatives.UnitTests/EmailAddress_Test.cs
@@ -0,0 +1,349 @@
+﻿using Moq;
+using System;
+using Xunit;
+
+namespace wimm.Secundatives.UnitTests
+{
+    public class Parser_Test
+    {
+        [Fact]
+        public void Make_ParseFails_ReturnsFalseAndResetsStream()
+        {
+            var stream = ResetableStream(42);
+
+            var underTest = Parser.Make(s => false);
+
+            AssertParseFailure(underTest, stream);
+        }
+
+        [Fact]
+        public void Make_ParseSucceeds_ReturnsTrueAndDoesNotResetStream()
+        {
+            var stream = new Mock<CharStream>();
+
+            var underTest = Parser.Make(s => true);
+
+            AssertParseSuccess(underTest, stream);
+        }
+
+        [Fact]
+        public void Make_StreamResetFails_Throws()
+        {
+            var stream = new Mock<CharStream>();
+
+            var underTest = Parser.Make(s => false);
+
+            Assert.Throws<Exception>(() => underTest(stream.Object));
+        }
+
+        [Fact]
+        public void Match_NextCharDoesNotMatch_ParseFails()
+        {
+            var stream = ResetableStream(42);
+            stream.Setup(s => s.Read()).Returns('b');
+
+            var underTest = Parser.Match('a');
+
+            AssertParseFailure(underTest, stream);
+        }
+
+        [Fact]
+        public void Match_NextCharMatches_ParseSucceeds()
+        {
+            var stream = new Mock<CharStream>();
+            stream.Setup(s => s.Read()).Returns('a');
+
+            var underTest = Parser.Match('a');
+
+            AssertParseSuccess(underTest, stream);
+        }
+
+        [Fact]
+        public void Complete_OriginalParseFails_ParseFails()
+        {
+            var stream = ResetableStream(42);
+
+            var underTest = new ParseFn(s => false).Complete();
+
+            AssertParseFailure(underTest, stream); 
+        }
+
+        [Fact]
+        public void Complete_ExtraCharactersInStream_ParseFails()
+        {
+            var stream = ResetableStream(42);
+            stream.Setup(s => s.Read()).Returns('a');
+
+            var underTest = new ParseFn(s => false).Complete();
+
+            AssertParseFailure(underTest, stream);
+        }
+
+        [Fact]
+        public void Complete_ParseConsumesEntireStream_ParseSucceeds()
+        {
+            var stream = new Mock<CharStream>();
+            // the stream will naturally return a None maybe from Read()
+
+            var underTest = new ParseFn(s => true).Complete();
+
+            AssertParseSuccess(underTest, stream);
+        }
+
+        [Fact]
+        public void Or_NeitherParseSucceeds_ParseFails()
+        {
+            var stream = ResetableStream(42);
+
+            var underTest = new ParseFn(s => false).Or(s => false);
+
+            AssertParseFailure(underTest, stream);
+        }
+
+        [Fact]
+        public void Or_FirstParseSucceeds_ParseSucceeds()
+        {
+            var stream = new Mock<CharStream>();
+
+            var underTest = new ParseFn(s => true).Or(s => false);
+
+            AssertParseSuccess(underTest, stream);
+        }
+
+        [Fact]
+        public void Or_SecondParseSucceeds_ParseSucceeds()
+        {
+            var stream = new Mock<CharStream>();
+
+            var underTest = new ParseFn(s => false).Or(s => true);
+
+            AssertParseSuccess(underTest, stream);
+        }
+
+        [Fact]
+        public void Then_FirstParseFails_ParseFails()
+        {
+            var stream = ResetableStream(42);
+
+            var underTest = new ParseFn(s => false).Then(s => true);
+
+            AssertParseFailure(underTest, stream);
+        }
+
+        [Fact]
+        public void Then_SecondParseFails_ParseFails()
+        {
+            var stream = ResetableStream(42);
+
+            var underTest = new ParseFn(s => true).Then(s => false);
+
+            AssertParseFailure(underTest, stream);
+        }
+
+        [Fact]
+        public void Then_BothParsesSucceed_ParseSucceeds()
+        {
+            var stream = ResetableStream(42);
+
+            var underTest = new ParseFn(s => true).Then(s => true);
+
+            AssertParseSuccess(underTest, stream);
+        }
+
+        [Fact]
+        public void Optional_Fails_ParseSucceedsWithReset()
+        {
+            var stream = ResetableStream(42);
+
+            var underTest = new ParseFn(s => false).Optional();
+
+            AssertParseSuccess(underTest, stream, 1);
+        }
+
+        [Fact]
+        public void Optional_Succeeds_ParseSucceeds()
+        {
+            var stream = new Mock<CharStream>();
+
+            var underTest = new ParseFn(s => true).Optional();
+
+            AssertParseSuccess(underTest, stream);
+        }
+
+        [Fact]
+        public void Repeat_ZeroTimes_ParseSucceeds()
+        {
+            var stream = new Mock<CharStream>();
+
+            var underTest = new ParseFn(s => false).Repeat(0);
+
+            AssertParseSuccess(underTest, stream);
+        }
+
+        [Fact]
+        public void Repeat_NotAllRepetitionsSucceed_ParseFails()
+        {
+            var stream = ResetableStream(42);
+
+            var parse = new Mock<ParseFn>();
+            parse.SetupSequence(s => s.Invoke(It.IsAny<CharStream>()))
+                .Returns(true)
+                .Returns(false);
+
+            var underTest = parse.Object.Repeat(3);
+
+            AssertParseFailure(underTest, stream);
+            parse.Verify(p => p.Invoke(It.IsAny<CharStream>()), Times.Exactly(2));
+        }
+        
+        [Fact]
+        public void Repeat_AllRepeitionsSucceed_ParseSucceeds()
+        {
+            var stream = new Mock<CharStream>();
+
+            var parse = new Mock<ParseFn>();
+            parse.Setup(s => s.Invoke(It.IsAny<CharStream>())).Returns(true);
+
+            var underTest = parse.Object.Repeat(3);
+
+            AssertParseSuccess(underTest, stream);
+            parse.Verify(p => p.Invoke(It.IsAny<CharStream>()), Times.Exactly(3));
+        }
+
+        private void AssertParseSuccess(ParseFn parse, Mock<CharStream> stream) =>
+            AssertParseSuccess(parse, stream, 0);
+
+        private void AssertParseSuccess(ParseFn parse, Mock<CharStream> stream, int streamResets)
+        {
+            Assert.True(parse(stream.Object));
+            stream.Verify(s => s.Seek(It.IsAny<long>()), Times.Exactly(streamResets));
+        }
+
+        private void AssertParseFailure(ParseFn parse, Mock<CharStream> stream) =>
+            AssertParseFailure(parse, stream, 1);
+
+        private void AssertParseFailure(ParseFn parse, Mock<CharStream> stream, int streamResets)
+        {
+            Assert.False(parse(stream.Object));
+            stream.Verify(s => s.Seek(It.IsAny<long>()), Times.Exactly(streamResets));
+        }
+
+        private Mock<CharStream> ResetableStream(long position)
+        {
+            var stream = new Mock<CharStream>();
+
+            stream.SetupGet(s => s.Position).Returns(position);
+            stream.Setup(s => s.Seek(position)).Returns(true);
+
+            return stream;
+        }
+    }
+
+    public class StringStream_Test
+    {
+        [Fact]
+        public void Construct_NullStr_Throws() =>
+            Assert.Throws<ArgumentNullException>(() => new StringStream(null));
+
+        [Fact]
+        public void Construct_NonNullStr_Constructs()
+        {
+            var _ = new StringStream("not null");
+        }
+
+        [Fact]
+        public void Position_NoReads_IsZero()
+        {
+            var underTest = new StringStream("some value");
+
+            Assert.Equal(0, underTest.Position);
+        }
+
+        [Fact]
+        public void Position_DuringSequentialRead_Increments()
+        {
+            var chars = "the characters to read";
+            var underTest = new StringStream(chars);
+
+            for (var i = 1; i <= chars.Length; i++)
+            {
+                var _ = underTest.Read();
+
+                Assert.Equal(i, underTest.Position);
+            }
+        }
+
+        [Fact]
+        public void Position_AfterAllReads_RemainsDataLength()
+        {
+            var chars = "the characters to read";
+            var underTest = new StringStream(chars);
+
+            for (var i = 1; i <= chars.Length; i++)
+            {
+                var __ = underTest.Read();
+            }
+
+            Assert.Equal(chars.Length, underTest.Position);
+            var _ = underTest.Read();
+            Assert.Equal(chars.Length, underTest.Position);
+        }
+
+        [Fact]
+        public void Read_StartToFinish_ReturnsExpectedChars()
+        {
+            var chars = "the characters to read";
+            var underTest = new StringStream(chars);
+
+            foreach (var c in chars)
+            {
+                Assert.Equal(c, underTest.Read().Value);
+            }
+        }
+
+        [Fact]
+        public void Read_AfterEnd_KeepaReturningNone()
+        {
+            var chars = "the characters to read";
+            var underTest = new StringStream(chars);
+
+            for (var i = 1; i <= chars.Length; i++)
+            {
+                var _ = underTest.Read();
+            }
+
+            Assert.Equal(Maybe<char>.None, underTest.Read());
+            Assert.Equal(Maybe<char>.None, underTest.Read());
+        }
+
+        [Fact]
+        public void Seek_OffsetOutOfBounds_ReturnsFalseAndDoesNotChangePosition()
+        {
+            var chars = "the characters to read";
+            var underTest = new StringStream(chars);
+
+            var _ = underTest.Read();
+            _ = underTest.Read();
+
+            Assert.False(underTest.Seek(chars.Length * 2));
+            Assert.Equal(2, underTest.Position);
+        }
+
+        [Fact]
+        public void Seek_Inbounds_SeeksToPosition()
+        {
+            var chars = "the characters to read";
+            var underTest = new StringStream(chars);
+
+            var p = chars.Length / 2;
+            underTest.Seek(p);
+
+            Assert.Equal(p, underTest.Position);
+
+            for (var i = p; i < chars.Length;  i++)
+            {
+                Assert.Equal(chars[i], underTest.Read().Value);
+            }
+        }
+    }
+}
---
+++ wimm.Secundatives/EmailAddress.cs
@@ -0,0 +1,216 @@
+﻿using System;
+
+namespace wimm.Secundatives
+{
+    /// <summary>
+    /// A validation type to represent an Rfc5322 compliant email address (addr-spec).
+    /// </summary>
+    public class EmailAddress
+    {
+        public EmailAddress(string localPart, string domain)
+        {
+            if (!(LocalPart(new StringStream(localPart)) && Domain(new StringStream(domain))))
+                throw new FormatException("baahahahd");
+
+            _localPart = localPart;
+            _domain = domain;
+        }
+
+        private string _localPart;
+        private string _domain;
+
+        /// <summary>
+        /// addr-spec = local-part "@" domain
+        /// </summary>
+        public static ParseFn AddrSpec =
+            LocalPart
+                .Then(Parser.Match('@'))
+                .Then(Domain);
+
+        /// <summary>
+        /// local-part = dot-atom / quoted-string / obs-local-part
+        /// </summary>
+        public static ParseFn LocalPart =
+            DotAtom
+                .Or(QuotedString)
+                .Or(ObsLocalPart);
+
+        /// <summary>
+        /// domain = dot-atom / domain-literal / obs-domain
+        /// </summary>
+        public static ParseFn Domain =
+             DotAtom
+                .Or(DomainLiteral)
+                .Or(ObsDomain);
+
+        /// <summary>
+        /// dot-atom = [CFWS] dot-atom-text [CFWS]
+        /// </summary>
+        public static ParseFn DotAtom =
+            (CFWS.Optional())
+                .Then(DotAtomText)
+                .Then((CFWS.Optional()));
+
+        public static ParseFn QuotedString = s => false;
+
+        public static ParseFn ObsLocalPart = s => false;
+
+        public static ParseFn DomainLiteral = s => false;
+
+        public static ParseFn ObsDomain = s => false;
+
+        public static ParseFn DotAtomText = s => false;
+
+
+
+        public static ParseFn CFWS = s => false;
+
+    }
+
+    /// <summary>
+    /// A set of <see cref="ParseFn"/> factories for common patterns.
+    /// </summary>
+    public static class Parser
+    {
+        /// <summary>
+        /// Makes a parser that encapsulates a parsing function and returns the incoming stream to
+        /// its original state if the parse function fails.
+        /// </summary>
+        /// <param name="parse">The parsing function.</param>
+        /// <returns>
+        /// A parser that uses the given parse function to parse a stream and returns the stream to
+        /// its original state if the parse function fails. If the stream cannot be returned to its
+        /// original position after a parse fails the returned parser will throw an
+        /// <see cref="Exception"/>.
+        /// </returns>
+        public static ParseFn Make(ParseFn parse)
+        {
+            return (s) =>
+            {
+                var position = s.Position;
+                var result = parse(s);
+
+                if (!result)
+                    if (!s.Seek(position))
+                        // TODO: Throw different exception
+                        throw new Exception($"CharStream could not Seek to the Position it started in ({position}).");
+
+                return result;
+            };
+        }
+
+        public static ParseFn Match(char c) => Make((s) => s.Read() == c);
+    }
+
+    /// <summary>
+    /// A function that consumes a specific pattern from an input stream and returns <c>true</c>,
+    /// or returns <c>false</c> and leaves the stream unchanged.
+    /// </summary>
+    /// <param name="input">The input stream.</param>
+    /// <returns>
+    /// <c>true</c> if the stream began with a match for the pattern being parsed, otherwise
+    /// <c>false</c>.
+    /// </returns>
+    public delegate bool ParseFn(CharStream input);
+
+    public static class ParserExtensions
+    {
+        public static ParseFn Complete(this ParseFn a) =>
+            Parser.Make(s => a(s) && !s.Read().Exists);
+
+        public static ParseFn Or(this ParseFn a, ParseFn b) => Parser.Make(s => a(s) || b(s));
+
+        public static ParseFn Then(this ParseFn a, ParseFn b) => Parser.Make(s => a(s) && b(s));
+
+        public static ParseFn Optional(this ParseFn parse) => s => Parser.Make(parse)(s) || true;
+
+        public static ParseFn Repeat(this ParseFn parse, int times)
+        {
+            return Parser.Make(s =>
+            {
+                for (var i = 0; i < times; i++)
+                {
+                    if (!parse(s))
+                        return false;
+                }
+
+                return true;
+            });
+        }
+
+    }
+
+    /// <summary>
+    /// A read-only, seekable stream of <see cref="char"/>s.
+    /// </summary>
+    public interface CharStream
+    {
+        /// <summary>
+        /// The position within the stream. When the whole stream has been read then
+        /// <see cref="Position"/> is equal to the length of the stream.
+        /// </summary>
+        long Position { get; }
+
+        /// <summary>
+        /// Reads a char from the stream and advances the position.
+        /// </summary>
+        /// <returns>
+        /// The next char in the stream, or <see cref="Maybe{CharStream}.None"/> if the whole
+        /// stream has already been read.
+        /// </returns>
+        Maybe<char> Read();
+
+        /// <summary>
+        /// Sets the read position of the stream to the specified offset. The offset must be
+        /// greater than or equal to 0 and less than or equal to the length of the data. An offset
+        /// of 0 will seek to the begining of the stream, 1 to the second char, etc. An offset
+        /// equal to the length of the data will seek past the data. An offset greater than the
+        /// legnth of the data will cause the function to fail and return false.
+        /// </summary>
+        /// <param name="offset">The position to seek to.</param>
+        /// <returns><c>true</c> if the operation succeeded, otherwise <c>false</c>.</returns>
+        bool Seek(long offset);
+    }
+
+    /// <summary>
+    /// A <see cref="CharStream"/> wrapper for a <see cref="string"/>.
+    /// </summary>
+    public class StringStream : CharStream
+    {
+        /// <summary>
+        /// Initializes a new <see cref="StringStream"/> for <paramref name="str"/>.
+        /// </summary>
+        /// <param name="str">The string to read.</param>
+        public StringStream(string str)
+        {
+            _str = str ?? throw new ArgumentNullException(nameof(str));
+            Position = 0;
+        }
+
+        /// <inheritDoc/>
+        public long Position { get; private set; }
+
+        /// <inheritDoc/>
+        public Maybe<char> Read()
+        {
+            if (Position == _str.Length)
+                return Maybe<char>.None;
+
+            char c = _str[(int)Position];
+            Position++;
+            return c;
+        }
+
+        /// <inheritDoc/>
+        public bool Seek(long offset)
+        {
+            if (offset < 0 || offset > _str.Length)
+                return false;
+
+            Position = offset;
+            return true;
+        }
+
+        private string _str;
+    }
+}
