7f7cd9a20310751e9a24bbdd88f739c339b4827d
Unknown <Chris.Nantau@gmail.com>
2019-11-03 23:50:48 -0400

    Added two useful and missing OkOr methods with tests and moved the namespace

--- wimm.Secundatives/Extensions/OkOrExtensions.cs
+++ wimm.Secundatives/Extensions/OkOrExtensions.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using wimm.Secundatives.Extensions;
 
 namespace wimm.Secundatives
 {
@@ -16,7 +14,7 @@ namespace wimm.Secundatives
         /// </summary>
         /// <typeparam name="T"> The value of the success type </typeparam>
         /// <typeparam name="TError"> The value of the error type created when <paramref name="value"/> contains no value </typeparam>
-        /// <param name="value"> The <paramref name="value"/> whose existence determines whether <see cref="Result{T, TError}"/> </param>
+        /// <param name="value"> The <paramref name="value"/> whose existence determines whether <see cref="Result{T, TError}"/> is a value or an error</param>
         /// <param name="func"> A function returning a <typeparamref name="TError"/> that is executed if <paramref name="value"/> is None</param>
         /// <returns> A <see cref="Result{T, TError}"/> containing the value of <paramref name="value"/> if it contains one, otherwise 
         /// contains the result of executing <paramref name="func"/>
@@ -37,7 +35,7 @@ namespace wimm.Secundatives
         /// </summary>
         /// <typeparam name="T"> The value of the success type </typeparam>
         /// <typeparam name="TError"> The value of the error type created when <paramref name="value"/> contains no value </typeparam>
-        /// <param name="value"> The <paramref name="value"/> whose existence determines whether <see cref="Result{T, TError}"/> </param>
+        /// <param name="value"> The <paramref name="value"/> whose existence determines whether <see cref="Result{T, TError}"/> is a value or an error</param>
         /// <param name="error"> The default value returned if <paramref name="value"/> is None </param>
         /// <returns> A <see cref="Result{T, TError}"/> containing the value of <paramref name="value"/> if it contains one, otherwise 
         /// contains <paramref name="error"/>
@@ -58,7 +56,7 @@ namespace wimm.Secundatives
         /// </summary>
         /// <typeparam name="T"> The value of the success type </typeparam>
         /// <typeparam name="TError"> The value of the error type created when <paramref name="value"/> contains no value </typeparam>
-        /// <param name="value"> The <paramref name="value"/> whose existence determines whether <see cref="Result{T, TError}"/> </param>
+        /// <param name="value"> The <paramref name="value"/> whose existence determines whether <see cref="Result{T, TError}"/> is a value or an error</param>
         /// <param name="error"> The default value returned if <paramref name="value"/> is None </param>
         /// <returns> A <see cref="Result{T, TError}"/> containing the value of <paramref name="value"/> if it contains one, otherwise 
         /// contains <paramref name="error"/>
@@ -76,7 +74,7 @@ namespace wimm.Secundatives
         /// </summary>
         /// <typeparam name="T"> The value of the success type </typeparam>
         /// <typeparam name="TError"> The value of the error type created when <paramref name="value"/> contains no value </typeparam>
-        /// <param name="value"> The <paramref name="value"/> whose existence determines whether <see cref="Result{T, TError}"/> </param>
+        /// <param name="value"> The <paramref name="value"/> whose existence determines whether <see cref="Result{T, TError}"/> is a value or an error</param>
         /// <param name="func"> A function returning a <typeparamref name="TError"/> that is executed if <paramref name="value"/> is None</param>
         /// <returns> A <see cref="Result{T, TError}"/> containing the value of <paramref name="value"/> if it contains one, otherwise 
         /// contains the result of executing <paramref name="func"/>
@@ -87,6 +85,43 @@ namespace wimm.Secundatives
             return v.OkOr(func);
 
         }
+
+
+        /// <summary>
+        /// Collapsing mapping of a  <see cref="Task{T}"/> containing a <see cref="Maybe{T}"/> that may hold a <see cref="Result{T,TErr}"/>
+        /// into a <see cref="Result{T, TError}"/> 
+        /// </summary>
+        /// <typeparam name="T"> The value of the success type </typeparam>
+        /// <typeparam name="TError"> The value of the error type created when <paramref name="value"/> contains no value </typeparam>
+        /// <param name="value"> The <paramref name="value"/> whose existence determines whether <see cref="Result{T, TError}"/> 
+        /// is a the provided result value or a new <see cref="Result{T, TError}"/> created from <paramref name="error"/></param>
+        /// <param name="error"> The default value mappted into a <see cref="Result{T, TError}"/> if <paramref name="value"/> is None </param>
+        /// <returns> A <see cref="Result{T, TError}"/> either contained by <paramref name="value"/> if it exists or created
+        /// from <paramref name="error"/> if it is None</returns>
+        public static async Task<Result<T, TError>> OkOr<T, TError>(this Task<Maybe<Result<T, TError>>> value, TError error)
+        {
+            var res = await value;
+            return res.UnwrapOr(error);
+        }
+
+
+        /// <summary>
+        /// Collapsing mapping of a  <see cref="Task{T}"/> containing a <see cref="Maybe{T}"/> that may hold a <see cref="Result{T,TErr}"/>
+        /// into a <see cref="Result{T, TError}"/> 
+        /// </summary>
+        /// <typeparam name="T"> The value of the success type </typeparam>
+        /// <typeparam name="TError"> The value of the error type created when <paramref name="value"/> contains no value </typeparam>
+        /// <param name="value"> The <paramref name="value"/> whose existence determines whether <see cref="Result{T, TError}"/> 
+        /// is a the provided result value or a new <see cref="Result{T, TError}"/> created from calling <paramref name="func"/></param>
+        /// <param name="func"> A function returning a <typeparamref name="TError"/> that is executed if <paramref name="value"/> is None</param>
+        /// <returns> A <see cref="Result{T, TError}"/> either contained by <paramref name="value"/> if it exists or created
+        /// from the results of calling <paramref name="func"/> if it is None</returns>
+        public static async Task<Result<T, TError>> OkOr<T, TError>(this Task<Maybe<Result<T, TError>>> value, Func<TError> func)
+        {
+            var res = await value;
+            return res.UnwrapOr(func());
+        }
+
 
     }
 }
