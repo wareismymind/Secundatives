2dc33e1defcb102ddd2f41bcffa133d704a9396f
Thomas Showers <tshowers@beyondtrust.com>
2021-10-22 15:35:30 -0300

    chore: Simpler XML docs on .Map extensions for Maybe and Task<Maybe>

--- wimm.Secundatives/Extensions/MappingExtensions.cs
+++ wimm.Secundatives/Extensions/MappingExtensions.cs
@@ -9,85 +9,72 @@ namespace wimm.Secundatives
     public static class MappingExtensions
     {
         /// <summary>
-        /// Transforms <paramref name="target"/> into a <see cref="Maybe{U}"/> by applying <paramref name="func"/> to
-        /// its <see cref="Maybe{T}.Value"/>. If <paramref name="target"/> is <see cref="Maybe.None"/> then
-        /// <paramref name="func"/> is not applied and <see cref="Maybe.None"/> is returned directly.
+        /// Creates a new <see cref="Maybe{U}"/> by applying the given function to the existing
+        /// <see cref="Maybe{T}"/>'s value. If the existing <see cref="Maybe{T}"/> is <see cref="Maybe.None"/> then the
+        /// function is not applied and <see cref="Maybe.None"/> is returned directly.
         /// </summary>
-        /// <typeparam name="T">The type to be mapped from.</typeparam>
-        /// <typeparam name="U">The type to be mapped to.</typeparam>
-        /// <param name="target">The <see cref="Maybe{T}"/> whose <see cref="Maybe{T}.Value"/> to transform.</param>
-        /// <param name="func">
-        /// The function to use to transform the <see cref="Maybe{T}.Value"/> of <paramref name="target"/>.
-        /// </param>
+        /// <typeparam name="T">The type of the original value.</typeparam>
+        /// <typeparam name="U">The type of the new value.</typeparam>
+        /// <param name="target">The <see cref="Maybe{T}"/> whose value will be tranformed.</param>
+        /// <param name="func">The function to transform the value.</param>
         /// <returns>
-        /// A <see cref="Maybe{U}"/> containing the value returned by <paramref name="func"/>, or
-        /// <see cref="Maybe.None"/> if <paramref name="target"/> is <see cref="Maybe.None"/>.
+        /// A <see cref="Maybe{U}"/> containing the value returned from <paramref name="func"/>, or
+        /// <see cref="Maybe.None"/> if the original <see cref="Maybe{T}"/> was <see cref="Maybe.None"/>.
         /// </returns>
         public static Maybe<U> Map<T, U>(this Maybe<T> target, Func<T, U> func) =>
             target.Exists
                 ? func(target.Value)
                 : Maybe<U>.None;
 
-
         /// <summary>
-        /// Transforms <paramref name="target"/> into a <see cref="Maybe{U}"/> by applying <paramref name="func"/> to
-        /// its <see cref="Maybe{T}.Value"/>. If <paramref name="target"/> is <see cref="Maybe.None"/> then
-        /// <paramref name="func"/> is not applied and <see cref="Maybe.None"/> is returned directly.
+        /// Creates a new <see cref="Maybe{U}"/> by applying the given function to the existing
+        /// <see cref="Maybe{T}"/>'s value. If the existing <see cref="Maybe{T}"/> is <see cref="Maybe.None"/> then the
+        /// function is not applied and <see cref="Maybe.None"/> is returned directly.
         /// </summary>
-        /// <typeparam name="T">The type to be mapped from.</typeparam>
-        /// <typeparam name="U">The type to be mapped to.</typeparam>
-        /// <param name="target">The <see cref="Maybe{T}"/> whose <see cref="Maybe{T}.Value"/> to transform.</param>
-        /// <param name="func">
-        /// The function to use to transform the <see cref="Maybe{T}.Value"/> of <paramref name="target"/>.
-        /// </param>
+        /// <typeparam name="T">The type of the original value.</typeparam>
+        /// <typeparam name="U">The type of the new value.</typeparam>
+        /// <param name="target">The <see cref="Maybe{T}"/> whose value will be tranformed.</param>
+        /// <param name="func">The function to create the new <see cref="Maybe{U}"/>.</param>
         /// <returns>
-        /// The <see cref="Maybe{U}"/> returned by <paramref name="func"/>, or <see cref="Maybe.None"/> if
-        /// <paramref name="target"/> is <see cref="Maybe.None"/>.
+        /// The <see cref="Maybe{U}"/> returned from <paramref name="func"/>, or <see cref="Maybe.None"/> if the
+        /// original <see cref="Maybe{T}"/> was <see cref="Maybe.None"/>.
         /// </returns>
         public static Maybe<U> Map<T, U>(this Maybe<T> target, Func<T, Maybe<U>> func) =>
             target.Exists
                 ? func(target.Value)
                 : Maybe<U>.None;
 
-
         /// <summary>
-        /// Transforms <paramref name="target"/> into a <see cref="Task{MU}"/> containing a <see cref="Maybe{U}"/> by
-        /// applying <paramref name="func"/> to its <see cref="Maybe{T}.Value"/>. If <paramref name="target"/> is
-        /// <see cref="Maybe.None"/> then <paramref name="func"/> is not applied and a <see cref="Task{M}"/> containing
-        /// <see cref="Maybe.None"/> is returned directly.
+        /// Creates a new <see cref="Maybe{U}"/> by applying the given function to the existing
+        /// <see cref="Maybe{T}"/>'s value. If the existing <see cref="Maybe{T}"/> is <see cref="Maybe.None"/> then the
+        /// function is not applied and <see cref="Maybe.None"/> is returned directly.
         /// </summary>
-        /// <typeparam name="T">The type to be mapped from.</typeparam>
-        /// <typeparam name="U">The type to be mapped to.</typeparam>
-        /// <param name="target">The <see cref="Maybe{T}"/> whose <see cref="Maybe{T}.Value"/> to transform.</param>
-        /// <param name="func">
-        /// The function to use to transform the <see cref="Maybe{T}.Value"/> of <paramref name="target"/>.
-        /// </param>
+        /// <typeparam name="T">The type of the original value.</typeparam>
+        /// <typeparam name="U">The type of the new value.</typeparam>
+        /// <param name="target">The <see cref="Maybe{T}"/> whose value will be tranformed.</param>
+        /// <param name="func">The function to transform the value.</param>
         /// <returns>
-        /// A <see cref="Task{MU}"/> containing a <see cref="Maybe{U}"/> containing the <see cref="Task{U}.Result"/> of
-        /// the <see cref="Task{U}"/> returned by <paramref name="func"/>, or a <see cref="Task{MU}"/> containing
-        /// <see cref="Maybe.None"/> if <paramref name="target"/> is <see cref="Maybe.None"/>.
+        /// A <see cref="Task{MU}"/> whose's result is a <see cref="Maybe{U}"/> containing the result of the
+        /// <see cref="Task{U}"/> returned from <paramref name="func"/>, or <see cref="Maybe.None"/> if the original
+        /// <see cref="Maybe{T}"/> was <see cref="Maybe.None"/>.
         /// </returns>
         public static async Task<Maybe<U>> Map<T, U>(this Maybe<T> target, Func<T, Task<U>> func) =>
             target.Exists
                 ? await func(target.Value)
                 : Maybe<U>.None;
 
-
         /// <summary>
-        /// Transforms <paramref name="target"/> into a <see cref="Task{MU}"/> containing a <see cref="Maybe{U}"/> by
-        /// applying <paramref name="func"/> to its <see cref="Maybe{T}.Value"/>. If <paramref name="target"/> is
-        /// <see cref="Maybe.None"/> then <paramref name="func"/> is not applied and a <see cref="Task{M}"/> containing
-        /// <see cref="Maybe.None"/> is returned directly.
+        /// Creates a new <see cref="Maybe{U}"/> by applying the given function to the existing
+        /// <see cref="Maybe{T}"/>'s value. If the existing <see cref="Maybe{T}"/> is <see cref="Maybe.None"/> then the
+        /// function is not applied and <see cref="Maybe.None"/> is returned directly.
         /// </summary>
-        /// <typeparam name="T">The type to be mapped from.</typeparam>
-        /// <typeparam name="U">The type to be mapped to.</typeparam>
-        /// <param name="target">The <see cref="Maybe{T}"/> whose <see cref="Maybe{T}.Value"/> to transform.</param>
-        /// <param name="func">
-        /// The function to use to transform the <see cref="Maybe{T}.Value"/> of <paramref name="target"/>.
-        /// </param>
+        /// <typeparam name="T">The type of the original value.</typeparam>
+        /// <typeparam name="U">The type of the new value.</typeparam>
+        /// <param name="target">The <see cref="Maybe{T}"/> whose value will be tranformed.</param>
+        /// <param name="func">The function to transform the value.</param>
         /// <returns>
-        /// The <see cref="Task{MU}"/> returned by <paramref name="func"/>, or a <see cref="Task{MU}"/> containing
-        /// <see cref="Maybe.None"/> if <paramref name="target"/> is <see cref="Maybe.None"/>.
+        /// The <see cref="Task{MU}"/> returned from <paramref name="func"/>, or <see cref="Maybe.None"/> if the
+        /// original <see cref="Maybe{T}"/> was <see cref="Maybe.None"/>.
         /// </returns>
         public static async Task<Maybe<U>> Map<T, U>(this Maybe<T> target, Func<T, Task<Maybe<U>>> func) =>
             target.Exists
@@ -95,26 +82,20 @@ namespace wimm.Secundatives
                 : Maybe<U>.None;
 
         /// <summary>
-        /// Transforms <paramref name="target"/> into a <see cref="Task{MU}"/> containing a <see cref="Maybe{U}"/> by
-        /// applying <paramref name="func"/> to the <see cref="Maybe{T}.Value"/> of its <see cref="Task{MT}.Result"/>.
-        /// If the <see cref="Task{MT}.Result"/> of <paramref name="target"/> is <see cref="Maybe.None"/> then
-        /// <paramref name="func"/> is not applied and a <see cref="Task{MU}"/> containing <see cref="Maybe.None"/> is
-        /// returned directly.
+        /// Creates a new <see cref="Maybe{U}"/> by applying the given function to the existing
+        /// <see cref="Maybe{T}"/>'s value. If the existing <see cref="Maybe{T}"/> is <see cref="Maybe.None"/> then the
+        /// function is not applied and <see cref="Maybe.None"/> is returned directly.
         /// </summary>
-        /// <typeparam name="T">The type to be mapped from.</typeparam>
-        /// <typeparam name="U">The type to be mapped to.</typeparam>
+        /// <typeparam name="T">The type of the original value.</typeparam>
+        /// <typeparam name="U">The type of the new value.</typeparam>
         /// <param name="target">
-        /// The <see cref="Task{MT}"/> containing the <see cref="Maybe{T}"/> whose <see cref="Maybe{T}.Value"/> to
-        /// transform.
+        /// A <see cref="Task{MT}"/> whose result is the <see cref="Maybe{T}"/> whose value will be tranformed.
         /// </param>
-        /// <param name="func">
-        /// The function to use to transform the <see cref="Maybe{T}.Value"/> of the <see cref="Task{MT}.Result"/> of
-        /// <paramref name="target"/>.
-        /// </param>
+        /// <param name="func">The function to transform the value.</param>
         /// <returns>
-        /// A <see cref="Task{MU}"/> containing a <see cref="Maybe{U}"/> containing the value returned by
-        /// <paramref name="func"/>, or a <see cref="Task{MU}"/> containing <see cref="Maybe.None"/> if
-        /// the <see cref="Task{MT}.Result"/> of <paramref name="target"/> is <see cref="Maybe.None"/>.
+        /// A <see cref="Task{MU}"/> whose result is a <see cref="Maybe{U}"/> containing the value returned from
+        /// <paramref name="func"/>, or <see cref="Maybe.None"/> if the original <see cref="Maybe{T}"/> was
+        /// <see cref="Maybe.None"/>.
         /// </returns>
         public static async Task<Maybe<U>> Map<T, U>(this Task<Maybe<T>> target, Func<T, U> func)
         {
@@ -122,28 +103,20 @@ namespace wimm.Secundatives
             return value.Map(func);
         }
 
-
         /// <summary>
-        /// Transforms <paramref name="target"/> into a <see cref="Task{MU}"/> containing a <see cref="Maybe{U}"/> by
-        /// applying <paramref name="func"/> to the <see cref="Maybe{T}.Value"/> of its <see cref="Task{MT}.Result"/>.
-        /// If the <see cref="Task{MT}.Result"/> of <paramref name="target"/>  is <see cref="Maybe.None"/> then
-        /// <paramref name="func"/> is not applied and a <see cref="Task{MU}"/> containing <see cref="Maybe.None"/> is
-        /// returned directly.
+        /// Creates a new <see cref="Maybe{U}"/> by applying the given function to the existing
+        /// <see cref="Maybe{T}"/>'s value. If the existing <see cref="Maybe{T}"/> is <see cref="Maybe.None"/> then the
+        /// function is not applied and <see cref="Maybe.None"/> is returned directly.
         /// </summary>
-        /// <typeparam name="T">The type to be mapped from.</typeparam>
-        /// <typeparam name="U">The type to be mapped to.</typeparam>
+        /// <typeparam name="T">The type of the original value.</typeparam>
+        /// <typeparam name="U">The type of the new value.</typeparam>
         /// <param name="target">
-        /// The <see cref="Task{MT}"/> containing the <see cref="Maybe{T}"/> whose <see cref="Maybe{T}.Value"/> to
-        /// transform.
+        /// A <see cref="Task{MT}"/> whose result is the <see cref="Maybe{T}"/> whose value will be tranformed.
         /// </param>
-        /// <param name="func">
-        /// The function to use to transform the <see cref="Maybe{T}.Value"/> of the <see cref="Task{MT}.Result"/> of
-        /// <paramref name="target"/>.
-        /// </param>
+        /// <param name="func">The function to create the new <see cref="Maybe{U}"/>.</param>
         /// <returns>
-        /// A <see cref="Task{MU}"/> containing the <see cref="Maybe{U}"/> returned by <paramref name="func"/>, or a
-        /// <see cref="Task{MU}"/> containing <see cref="Maybe.None"/> if the <see cref="Task{MT}.Result"/> of
-        /// <paramref name="target"/> is <see cref="Maybe.None"/>.
+        /// A <see cref="Task{MU}"/> whose result is the <see cref="Maybe{U}"/> returned from <paramref name="func"/>,
+        /// or <see cref="Maybe.None"/> if the original <see cref="Maybe{T}"/> was <see cref="Maybe.None"/>.
         /// </returns>
         public static async Task<Maybe<U>> Map<T, U>(this Task<Maybe<T>> target, Func<T, Maybe<U>> func)
         {
@@ -151,29 +124,21 @@ namespace wimm.Secundatives
             return value.Map(func);
         }
 
-
         /// <summary>
-        /// Transforms <paramref name="target"/> into a <see cref="Task{MU}"/> containing a <see cref="Maybe{U}"/> by
-        /// applying <paramref name="func"/> to the <see cref="Maybe{T}.Value"/> of its <see cref="Task{MT}.Result"/>.
-        /// If the <see cref="Task{MT}.Result"/> of <paramref name="target"/> is <see cref="Maybe.None"/> then
-        /// <paramref name="func"/> is not applied and a <see cref="Task{M}"/> containing <see cref="Maybe.None"/> is
-        /// returned directly.
+        /// Creates a new <see cref="Maybe{U}"/> by applying the given function to the existing
+        /// <see cref="Maybe{T}"/>'s value. If the existing <see cref="Maybe{T}"/> is <see cref="Maybe.None"/> then the
+        /// function is not applied and <see cref="Maybe.None"/> is returned directly.
         /// </summary>
-        /// <typeparam name="T">The type to be mapped from.</typeparam>
-        /// <typeparam name="U">The type to be mapped to.</typeparam>
+        /// <typeparam name="T">The type of the original value.</typeparam>
+        /// <typeparam name="U">The type of the new value.</typeparam>
         /// <param name="target">
-        /// The <see cref="Task{MT}"/> containing the <see cref="Maybe{T}"/> whose <see cref="Maybe{T}.Value"/> to
-        /// transform.
+        /// A <see cref="Task{MT}"/> whose result is the <see cref="Maybe{T}"/> whose value will be tranformed.
         /// </param>
-        /// <param name="func">
-        /// The function to use to transform the <see cref="Maybe{T}.Value"/> of the <see cref="Task{MT}.Result"/> of
-        /// <paramref name="target"/>.
-        /// </param>
+        /// <param name="func">The function to transform the value.</param>
         /// <returns>
-        /// A <see cref="Task{MU}"/> containing a <see cref="Maybe{U}"/> containing the <see cref="Task{U}.Result"/> of
-        /// the <see cref="Task{U}"/> returned by <paramref name="func"/>, or a <see cref="Task{MU}"/> containing
-        /// <see cref="Maybe.None"/> if the <see cref="Task{MT}.Result"/> of <paramref name="target"/> is
-        /// <see cref="Maybe.None"/>.
+        /// A <see cref="Task{MU}"/> whose's result is a <see cref="Maybe{U}"/> containing the result of the
+        /// <see cref="Task{U}"/> returned from <paramref name="func"/>, or <see cref="Maybe.None"/> if the original
+        /// <see cref="Maybe{T}"/> was <see cref="Maybe.None"/>.
         /// </returns>
         public static async Task<Maybe<U>> Map<T, U>(this Task<Maybe<T>> target, Func<T, Task<U>> func)
         {
@@ -182,26 +147,19 @@ namespace wimm.Secundatives
         }
 
         /// <summary>
-        /// Transforms <paramref name="target"/> into a <see cref="Task{MU}"/> containing a <see cref="Maybe{U}"/> by
-        /// applying <paramref name="func"/> to the <see cref="Maybe{T}.Value"/> of its <see cref="Task{MT}.Result"/>.
-        /// If the <see cref="Task{MT}.Result"/> of <paramref name="target"/> is <see cref="Maybe.None"/> then
-        /// <paramref name="func"/> is not applied and a <see cref="Task{MU}"/> containing <see cref="Maybe.None"/> is
-        /// returned directly.
+        /// Creates a new <see cref="Maybe{U}"/> by applying the given function to the existing
+        /// <see cref="Maybe{T}"/>'s value. If the existing <see cref="Maybe{T}"/> is <see cref="Maybe.None"/> then the
+        /// function is not applied and <see cref="Maybe.None"/> is returned directly.
         /// </summary>
-        /// <typeparam name="T">The type to be mapped from.</typeparam>
-        /// <typeparam name="U">The type to be mapped to.</typeparam>
+        /// <typeparam name="T">The type of the original value.</typeparam>
+        /// <typeparam name="U">The type of the new value.</typeparam>
         /// <param name="target">
-        /// The <see cref="Task{MT}"/> containing the <see cref="Maybe{T}"/> whose <see cref="Maybe{T}.Value"/> to
-        /// transform.
+        /// A <see cref="Task{MT}"/> whose result is the <see cref="Maybe{T}"/> whose value will be tranformed.
         /// </param>
-        /// <param name="func">
-        /// The function to use to transform the <see cref="Maybe{T}.Value"/> of the <see cref="Task{MT}.Result"/> of
-        /// <paramref name="target"/>.
-        /// </param>
+        /// <param name="func">The function to transform the value.</param>
         /// <returns>
-        /// The <see cref="Task{MU}"/> returned by <paramref name="func"/>, or a <see cref="Task{MU}"/> containing
-        /// <see cref="Maybe.None"/> if the <see cref="Task{MT}.Result"/> of <paramref name="target"/> is
-        /// <see cref="Maybe.None"/>.
+        /// The <see cref="Task{MU}"/> returned from <paramref name="func"/>, or <see cref="Maybe.None"/> if the
+        /// original <see cref="Maybe{T}"/> was <see cref="Maybe.None"/>.
         /// </returns>
         public static async Task<Maybe<U>> Map<T, U>(this Task<Maybe<T>> target, Func<T, Task<Maybe<U>>> func)
         {
