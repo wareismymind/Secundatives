d14c24403118c29572b61b4d649ab1a2388eed7e
sl4gr00m@gmail.com <ttd2089@gmail.com>
2021-10-22 17:55:29 -0300

    chore: Use Map(..., Func<T, Maybe<U>>) overload

--- wimm.Secundatives/EnumerableExtensions/EnumerableExtensions.cs
+++ wimm.Secundatives/EnumerableExtensions/EnumerableExtensions.cs
@@ -32,7 +32,7 @@ namespace wimm.Secundatives.EnumerableExtensions
         /// <param name="func">The function to use to transform the enumerable's values.</param>
         /// <returns>An enumerable of the transformed values.</returns>
         public static IEnumerable<Maybe<U>> Map<T, U>(this IEnumerable<Maybe<T>> target, Func<T, Maybe<U>> func) =>
-            target.Select(x => x.Map(func)); // todo: test
+            target.Select(x => x.Map(func));
 
         /// <summary>
         /// Tranforms the elements of the enumerable using the supplied function. Elements that are
@@ -44,7 +44,7 @@ namespace wimm.Secundatives.EnumerableExtensions
         /// <param name="func">The function to use to transform the enumerable's values.</param>
         /// <returns>An enumerable of the transformed values.</returns>
         public static IEnumerable<Task<Maybe<U>>> Map<T, U>(this IEnumerable<Maybe<T>> target, Func<T, Task<U>> func) =>
-            target.Select(x => x.Map(func)); // todo: test
+            target.Select(x => x.Map(func));
 
         /// <summary>
         /// Tranforms the elements of the enumerable using the supplied function. Elements that are
@@ -56,7 +56,7 @@ namespace wimm.Secundatives.EnumerableExtensions
         /// <param name="func">The function to use to transform the enumerable's values.</param>
         /// <returns>An enumerable of the transformed values.</returns>
         public static IEnumerable<Task<Maybe<U>>> Map<T, U>(this IEnumerable<Maybe<T>> target, Func<T, Task<Maybe<U>>> func) =>
-            target.Select(x => x.Map(func)); // todo: test
+            target.Select(x => x.Map(func));
 
         /// <summary>
         /// Tranforms the elements of the enumerable using the supplied function. Elements that are
@@ -80,15 +80,7 @@ namespace wimm.Secundatives.EnumerableExtensions
         /// <param name="func">The function to use to transform the enumerable's values.</param>
         /// <returns>An enumerable of the transformed values.</returns>
         public static IEnumerable<Task<Maybe<U>>> Map<T, U>(this IEnumerable<Task<Maybe<T>>> target, Func<T, Maybe<U>> func) =>
-            // todo: Use  Map<T, U>(this Task<Maybe<T>>, Func<T, Maybe<U>>) when
-            // https://github.com/wareismymind/Secundatives/pull/56/files is merged.
-            target.Select(async (task) =>
-            {
-                var maybe = await task;
-                return maybe.Exists
-                    ? func(maybe.Value)
-                    : Maybe.None;
-            });
+            target.Select(x => x.Map(func));
 
         /// <summary>
         /// Tranforms the elements of the enumerable using the supplied function. Elements that are
